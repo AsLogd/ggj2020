72add7e149fdea6e108375b5dba4f774ae2b814b
aslogd <gerarddelcastillo@gmail.com>
2020-02-01 19:37:59 +0100

    fix format

--- src/App.tsx
+++ src/App.tsx
@@ -11,82 +11,82 @@ import { AppContext } from "./AppContext"
 import "./style.scss"
 
 export const enum AppView {
-    MAIN_MENU,
-    GAME,
-    ABOUT
+	MAIN_MENU,
+	GAME,
+	ABOUT
 }
 
 interface AppState {
-    view: AppView
+	view: AppView
 }
 
 class App extends React.Component<{}, AppState> {
-    state = {
-	view: AppView.MAIN_MENU
-    }
-
-    componentDidUpdate(){
-	hotkeys("esc", (event) => {
-	    this.changeView(AppView.MAIN_MENU)
-	})
-    }
-
-    changeView = (view: AppView) => {
-	this.setState({ view })
-    }
-
-    contextImp() {
-	return {
-	    changeView: this.changeView
+	state = {
+		view: AppView.MAIN_MENU
 	}
-    }
 
-    renderViewElement() {
-	const { view } = this.state
-	const isOpen = {
-	    menu: view === AppView.MAIN_MENU,
-	    game: view === AppView.GAME,
-	    about: view === AppView.ABOUT,
+	componentDidUpdate() {
+		hotkeys("esc", (event) => {
+			this.changeView(AppView.MAIN_MENU)
+		})
 	}
-	
-	return(
-		<div className="App-component__view-container">
-		<AppContext.Provider value={this.contextImp()}>
-		<Toggle open={isOpen.menu} className="App-component__view">
-		<Menu />
-		</Toggle>
-		<Toggle open={isOpen.game} className="App-component__view">
-		<Repair />
-		</Toggle>
-		<Toggle open={isOpen.about} className="App-component__view App-component__view--about">
-		<h1>The dudes</h1>
-		<p>Aslo</p>
-		<p>Marcelino</p>
-		<p>Lali cama</p>
-		<p>Veuske</p>
-		</Toggle>
-		</AppContext.Provider>
-		</div>
-	)
-    }
 
-    render() {
-	return (
-		<div className="App-component">
-		    <div className="App-component__view-container">
-		<div className="App-component__view">
-		<Repair />
-		</div>
-		</div>
-		</div>
-	)
+	changeView = (view: AppView) => {
+		this.setState({ view })
+	}
 
-	return (
-		<div className="App-component">
-		{ this.renderViewElement() }
-	    </div>
-	)
-    }
+	contextImp() {
+		return {
+			changeView: this.changeView
+		}
+	}
+
+	renderViewElement() {
+		const { view } = this.state
+		const isOpen = {
+			menu: view === AppView.MAIN_MENU,
+			game: view === AppView.GAME,
+			about: view === AppView.ABOUT,
+		}
+
+		return (
+			<div className="App-component__view-container">
+				<AppContext.Provider value={this.contextImp()}>
+					<Toggle open={isOpen.menu} className="App-component__view">
+						<Menu />
+					</Toggle>
+					<Toggle open={isOpen.game} className="App-component__view">
+						<Repair />
+					</Toggle>
+					<Toggle open={isOpen.about} className="App-component__view App-component__view--about">
+						<h1>The dudes</h1>
+						<p>Aslo</p>
+						<p>Marcelino</p>
+						<p>Lali cama</p>
+						<p>Veuske</p>
+					</Toggle>
+				</AppContext.Provider>
+			</div>
+		)
+	}
+
+	render() {
+		return (
+			<div className="App-component">
+				<div className="App-component__view-container">
+					<div className="App-component__view">
+						<Repair />
+					</div>
+				</div>
+			</div>
+		)
+
+		return (
+			<div className="App-component">
+				{this.renderViewElement()}
+			</div>
+		)
+	}
 }
 
 render(<App />, document.getElementById('app'));
--- src/components/Game/jigsaw.ts
+++ src/components/Game/jigsaw.ts
@@ -6,8 +6,8 @@ import { potato } from "./assets"
 
 export default class JigsawPuzzle extends Screen {
     constructor(game, pos, size) {
-	const keys = [Key.Up, Key.Down, Key.Left, Key.Right]
-	super(game, pos, size, MinigameType.JIGSAW_PUZZLE, keys)
+        const keys = [Key.Up, Key.Down, Key.Left, Key.Right]
+        super(game, pos, size, MinigameType.JIGSAW_PUZZLE, keys)
     }
 
     update(dt: number) {
@@ -15,11 +15,11 @@ export default class JigsawPuzzle extends Screen {
     }
 
     draw_game() {
-	// this.game.canvas.fillStyle = 'red'
-	// this.game.canvas.fillRect(this.pos[0], this.pos[1], this.size[0], this.size[1])
+        // this.game.canvas.fillStyle = 'red'
+        // this.game.canvas.fillRect(this.pos[0], this.pos[1], this.size[0], this.size[1])
     }
 
     event(key: Key) {
-	// TODO(Marce): 
+        // TODO(Marce): 
     }
 }
